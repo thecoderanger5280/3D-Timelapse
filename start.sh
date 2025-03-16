09b4e2755727a78c015abc3f09cd2f435e5bc2a4
Ryan Loughry <108746864+thecoderanger5280@users.noreply.github.com>
2025-03-16 16:52:54 -0600

    Add files via upload

--- start.sh
+++ start.sh
@@ -5,20 +5,20 @@
 #pinctrl set 27 ip pu
 
 topic_name="esp32/data"
-user="user"
-pass="pass"
+user="onua5280"
+pass="Maxldr23"
 
 d=0
 
 #while true; do
   #if [ $(pinctrl lev 22) == 0 ]; then
-    #mkdir /path/to/file/t$d
+    #mkdir /home/onua5280/t$d
     #n=0
     #. ./save_pic.sh
     #while true; do
       #if [ $(pinctrl lev 17) == 0 ]; then
         #sleep 0.1
-        #libcamera-jpeg -t 1 -o /path/to/file/t$d/pic$n.jpg --nopreview
+        #libcamera-jpeg -t 1 -o /home/onua5280/t$d/pic$n.jpg --nopreview
         #((n++))
         #sleep 0.1
       #fi
@@ -33,14 +33,18 @@ d=0
 while true; do
   while read data; do
     if [ ${data:0:1} == 0 ]; then
-      mkdir -p /path/to/file/timelapse$d
+      mkdir -p /home/onua5280/timelapse$d
       p=0
       while read data1; do
         if [ ${data1:1:1} == 0 ]; then
-          libcamera-still -t .11 -o /path/to/file/timelapse$d/pic$p.jpg
-          ((p++))
+          libcamera-still -t .1 -o /home/onua5280/timelapse$d/pic$p.jpg
+          if [ -f /home/onua5280/timelapse$d/pic$p.jpg ]; then
+            ((p++))
+          fi
         fi
         if [ ${data1:2:1} == 0 ]; then
+          ffmpeg -y -framerate 10 -i /home/onua5280/timelapse0/pic%d.jpg /home/onua5280/timelapse$d.mp4
+          rm -rf /home/onua5280/timelapse$d
           break
         fi
       done < <(mosquitto_sub -t $topic_name -u $user -P $pass)
